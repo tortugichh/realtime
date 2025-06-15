f4c29a9a10c1746b8fccc809377c84c86e94f8af
tortugichh <otesh0621@gmail.com>
2025-06-15 16:12:37 +0500

    cors

--- main.py
+++ main.py
@@ -4,11 +4,24 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from fastapi import FastAPI, File, UploadFile, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from typing import Dict, List
 import chat
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:5173",  # Your frontend's origin
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class ChatResponse(BaseModel):
     audio_content: str
